2862608e4affa093ebab4dd68279b9f4e1174a5c
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-04-06 12:09:59 +0500

    first commiit

---
+++ main.js
@@ -0,0 +1,72 @@
+// 17-masala
+
+//  var mergeTwoLists = function(list1, list2) {
+//     let arr = [...list1,...list2]
+//     let new_arr = arr.sort()
+//     console.log(new_arr);
+// };
+// mergeTwoLists(list1 = [1,2,4], list2 = [1,3,4])
+
+
+
+
+
+
+
+
+
+
+// 18-masala
+
+// let arr = [9,3,1,2,7,5,4,4]
+// let new_arr = []
+// let total = []
+// for(let i = 0; i < arr.length; i++){
+//     if(!new_arr.includes(arr[i])){
+//         new_arr.push(arr[i])
+//     }else{
+//         massiv.push(arr[i])
+//     }
+// }
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+// 19-masala
+// var reverse = function(x) {
+//     let item = x
+//     let arr = Array.from(item.toString(), Number).reverse()
+//     let result = arr.join("")
+//     console.log(result);
+// };
+// reverse(4675)
+
+
+
+
+
+
+
+
+
+
+
+// 20-masala 
+
+// function fn (array){
+//     let arr = parseInt(array.length / 2)
+//     if(array.length % 2 == 0){
+//         let a = (array[arr] + (array[arr] - 1)) / 2
+//         return a
+//     }else {
+//         return array[arr]
+//     }
+// }
+// console.log(fn([1,2,3,4,5]));
